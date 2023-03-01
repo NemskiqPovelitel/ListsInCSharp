57beb90efbe06e4eba77f1caec499a7cb4028678
STUDENTS-1212-13 <vladtepes1048@gmail.com>
2023-03-01 11:22:58 +0200

    Add project files.

---
+++ Fibonachi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonachi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> fib = new List<int>();
+            fib.Add(1);
+            fib.Add(1);
+            Console.Write("do koe chislo na fibonachi iskash da vkarash" +
+                "\n N= ");
+            byte broika = byte.Parse(Console.ReadLine());
+            for (int i = 2; i < broika; i++)
+            {
+                fib.Add(fib[i - 2] + fib[i - 1]);
+            }
+            Console.WriteLine(string.Join(", ",fib));
+        }
+    }
+}
+
---
+++ zadacha1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vuvedi broi elementi: ");
+            int n = int.Parse(Console.ReadLine());
+            List<int>numbers=new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Vuvedi chislo: ");
+                int item = int.Parse(Console.ReadLine());
+                numbers.Add(item);
+
+            }
+            //int newIndex = n / 2;
+            //numbers.Insert(newIndex, 6);
+
+            //int minElement = numbers.Min();
+            //numbers.Add(minElement);
+            //foreach (int chervenolvadratche in numbers)
+            while(numbers.Contains(0))
+            {
+                numbers.Remove(0);
+            }
+           
+            
+
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine($"numbers[{i}]={numbers[i]}");
+            }
+            Console.WriteLine("Broqt na elementite v spisuka e: "+ numbers.Count);
+        }
+    }
+}
